14fff34f680830f2d65b4e3108be8357d812d1e2
nurultaukid13 <nurultaukid17@gmail.com>
2023-03-31 17:21:13 +0700

    taukid

--- pemilihanjurusan.py
+++ pemilihanjurusan.py
@@ -644,5 +644,9 @@ def preferensi_kerja(nilai, minat, bakat):
 nilai=nilai_akademik()
 minat= minat(nilai)
 bakat= bakat(nilai, minat)
+<<<<<<< HEAD
 preferensi_kerja(nilai, minat, bakat)
 ubah
+=======
+preferensi_kerja(nilai, minat, bakat, ubah)
+>>>>>>> 53eb475 (taukid)
